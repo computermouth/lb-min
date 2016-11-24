417585df22fd4e3382ce72e948ac8e67d6ca213f
computermouth <computermouth@crunchbangplusplus.org>
2016-11-24 00:07:19 -0800

    Move it all to one build step

--- build.sh
+++ build.sh
@@ -10,3 +10,15 @@ rm rootfs.tar
 
 pushd binary
 sudo tar -cf ../rootfs.tar .
+
+
+CHIP_UBOOT_BRANCH=${CHIP_UBOOT_BRANCH:-production-mlc}
+
+git clone https://github.com/nextthingco/chip-u-boot
+pushd chip-u-boot
+
+git checkout ${CHIP_UBOOT_BRANCH}
+
+make ${UBOOT_EXTRA_OPTS} ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- CHIP_defconfig
+make ${UBOOT_EXTRA_OPTS} ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j8
+
